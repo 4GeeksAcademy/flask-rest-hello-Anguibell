85b6e9aeae0762c34bd5e4a8c3f5e950039a7b32
Anguibell <romeroanguibell17@gmail.com>
2024-09-14 03:19:00 +0000

    New changes

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Character, Favorite
 #from models import Person
 
 app = Flask(__name__)
@@ -38,12 +38,83 @@ def sitemap():
 
 @app.route('/user', methods=['GET'])
 def handle_hello():
+    users = User.query.all()
+    usuarios_serializados = [ persona.serialize() for persona in users ]
+    return jsonify(usuarios_serializados), 200
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+@app.route('/user', methods=['POST'])
+def add_user():
+    body = request.json
+    username = body.get('username', None)
+    email = body.get('email', None)
+    password = body.get('password', None)
 
-    return jsonify(response_body), 200
+    if username == None or email == None or password == None:
+        return jsonify({"msg": "Missing fields"}), 400
+    
+    try:
+            
+        new_user = User(email=email, username=username, password=password)
+        db.session.add(new_user)
+        db.session.commit()
+        return jsonify({"msg": "success"}), 201
+
+    except:
+        return jsonify({"error": "Somethings happened unexpectedly"}), 500
+    
+@app.route('/user/<string:username>', methods=['DELETE'])
+def remove_user(username):
+    searched_user = User.query.filter_by(username=username).one_or_none()
+    if searched_user != None:
+        db.session.delete(searched_user)
+        db.session.commit()
+        return jsonify(searched_user.serialize()), 200
+    else:
+        return jsonify({"error": f"User with username: {username} not found!"}), 404
+    
+@app.route('/user/<string:username>', methods=['PUT'])
+def update_user(username):
+    body = request.json
+    new_username = body.get('username', None)
+    password = body.get('password', None)
+
+    searched_user = User.query.filter_by(username=username).one_or_none()
+    if searched_user != None:
+        if new_username != None:
+            searched_user.username = new_username
+        
+        if password != None:
+            searched_user.password = password
+
+        db.session.commit()
+        return jsonify(searched_user.serialize()), 200
+    else:
+        return jsonify({"error": f"User with username: {username} not found!"}), 404
+
+@app.route('/favorites', methods=['GET'])
+def get_favorites():
+    favorites = Favorite.query.all()
+    fav_serializados = [ favorite.serialize() for favorite in favorites ]
+    return jsonify(fav_serializados), 200
+
+@app.route('/favorites', methods=['POST'])
+def get_favorites():
+    body = request.json
+    user_id = body.get('user_id', None)
+    character_id = body.get('character_id', None)
+
+    if user_id == None or character_id == None:
+        return jsonify({"error": f"Missing with id {user_id} or Character with id {character_id} not found"}), 400
+
+    user = User.query.get(user_id)
+    character = Character.query.get(character_id)
+
+    if user == None or character == None:
+        return jsonify({"error": f"Missing with id {user_id} or Character with id {character_id} not found"}), 400
+    new_favorite = Favorite(user, character)
+    db.session.add(new_favorite)
+    db.session.commit()
+    return jsonify(new_favorite.serialize()), 200
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -5,15 +5,61 @@ db = SQLAlchemy()
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
+    username = db.Column(db.String(12), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
+    def __init__ (self, email, password, username):
+        self.email = email
+        self.password = password
+        self.username = username 
+        self.is_active = True
+
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<Usuario %r>' % self.username
+    
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
+            "username": self.username,
             # do not serialize the password, its a security breach
+        }
+    
+class Character(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+
+    name = db.Column(db.String(42), unique=True, nullable=False)
+    ki = db.Column(db.Integer, nullable=False)
+    race = db.Column(db.String(48), nullable=False)
+
+    def __init__(self, name, ki, race):
+        self.name = name
+        self.ki = ki
+        self.race = race
+
+    def serialize(self):
+        return {
+            "name": self.name,
+            "ki": self.ki,
+            "race": self.race,
+        }
+
+class Favorite(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('User.id'), nullable=False)
+    user = db.relationship("User")
+    character_id = db.Column(db.Integer, db.ForeignKey('Character.id'), nullable=False)
+    character = db.relationship("Character")
+
+    def __init__(self, user, character):
+        self.user = user
+        self.character = character
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user": self.user.serialize(),
+            "character": self.character.serialize(),
         }
